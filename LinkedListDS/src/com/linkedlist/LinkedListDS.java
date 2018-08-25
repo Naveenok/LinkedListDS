7995eb3b82de6f33db2ebf075c634dd65c62b065
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2018-08-25 16:24:44 +0530

    insertATPosition method code changes

--- LinkedListDS/src/com/linkedlist/LinkedListDS.java
+++ LinkedListDS/src/com/linkedlist/LinkedListDS.java
@@ -4,59 +4,58 @@
 package com.linkedlist;
 
 public class LinkedListDS {
-Node head;
+	Node head;
 
-public void insert(int data) {
-	Node n=new Node();
-	n.data=data;
-	n.next=null;
-	if(head==null){
-		head=n;
-	}
-	else {
-		Node n1=head;
-		while(n1.next!=null){
-			n1=n1.next;
+	public void insert(int data) {
+		Node n = new Node();
+		n.data = data;
+		n.next = null;
+		if (head == null) {
+			head = n;
+		} else {
+			Node n1 = head;
+			while (n1.next != null) {
+				n1 = n1.next;
+			}
+			n1.next = n;
 		}
-		n1.next=n;
+
 	}
 
-}
-
-public void show(){
-	Node node=head;
-	while(node.next!=null){
+	public void show() {
+		Node node = head;
+		while (node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
 		System.out.println(node.data);
-		node=node.next;
 	}
-	System.out.println(node.data);
-}
-public void insertAtStart(int value){
-	Node obj=new Node();
-	obj.data=value;
-	obj.next=null;
-	if(head==null){
-		head=obj;
-	}
-	else {
-		obj.next=head;
-		head=obj;
-	}
-}
-public void insertAtPosition(int loc,int value) {
-	Node obj = new Node();
-	obj.data=value;
-	obj.next=null;
-	Node node=head;
-	if (loc==0) {
-		insertAtStart(value);
-	}
-	for (int i = 0; i < loc-1; i++) {
-		node=node.next;
-	}	
-	obj.next=node.next;
-	node.next=obj;
-	}
-	
-}
 
+	public void insertAtStart(int value) {
+		Node obj = new Node();
+		obj.data = value;
+		obj.next = null;
+		if (head == null) {
+			head = obj;
+		} else {
+			obj.next = head;
+			head = obj;
+		}
+	}
+
+	public void insertAtPosition(int loc, int value) {
+		Node obj = new Node();
+		obj.data = value;
+		obj.next = null;
+		Node node = head;
+		if (loc == 0) {
+			insertAtStart(value);
+		} else {
+			for (int i = 0; i < loc - 1; i++) {
+				node = node.next;
+			}
+			obj.next = node.next;
+			node.next = obj;
+		}
+	}
+}
